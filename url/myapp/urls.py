3a10900b1681ba17f723a9560a5ab474756603ef
Pratik Pimpalkar <pratikpimpalkar21@gmail.com>
2022-04-06 23:21:08 +0530

    basic functionality done

---
+++ url/myapp/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+
+from . import views
+
+urlpatterns = [
+   
+    path('', views.home_page),
+    path('task', views.task),
+    path('all-analytics', views.all_analytics),
+    path('<slug:short_url>', views.redirect_url),
+    path('analytics/<slug:short_url>', views.redirect_url)
+]
